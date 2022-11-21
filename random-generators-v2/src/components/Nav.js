0122f0780eab159809500bb3df05140459fee7cb
0xromrom <roman0811@live.nl>
2022-11-21 13:25:53 -0800

    Mock nav added

---
+++ random-generators-v2/src/components/Nav.js
@@ -0,0 +1,16 @@
+import cl from "./Nav.module.css";
+
+const Nav = () => {
+  return (
+    <ul className={cl.nav}>
+      <li className={cl["header-text"]}>Random Generators</li>
+      <li>Number</li>
+      <li>Letter</li>
+      <li>Word</li>
+      <li>Country</li>
+      <li>Coin Flip</li>
+    </ul>
+  );
+};
+
+export default Nav;
