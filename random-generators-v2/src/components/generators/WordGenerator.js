8f45ac4342a9ae848a94f34ad05d6efa18e9c744
0xromrom <roman0811@live.nl>
2022-11-22 11:42:10 -0800

    Word filter added

--- random-generators-v2/src/components/generators/WordGenerator.js
+++ random-generators-v2/src/components/generators/WordGenerator.js
@@ -1,5 +1,5 @@
 import cl from "./WordGenerator.module.css";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 const words = require("an-array-of-english-words");
@@ -7,6 +7,8 @@ const words = require("an-array-of-english-words");
 const WordGenerator = () => {
   const [logArray, setLogArray] = useState([]);
   const [randomWord, setRandomWord] = useState("");
+  const includeRef = useRef("");
+
   const deleteListHandler = () => {
     setLogArray([]);
     setRandomWord("");
@@ -14,8 +16,21 @@ const WordGenerator = () => {
 
   const wordHandler = () => {
     const randomWord = Math.round(Math.random() * 274937);
+    if (includeRef.current.value !== "") {
+      const userWord = new RegExp(includeRef.current.value, "i");
+      const filteredWords = words.filter((d) => userWord.test(d));
+      const randomWords = Math.round(Math.random() * filteredWords.length);
+      const resultWord =
+        filteredWords[randomWords].charAt(0).toUpperCase() +
+        filteredWords[randomWords].slice(1);
+      setRandomWord(resultWord);
+      setLogArray((word) => [...word, resultWord]);
+      return;
+    }
+
     const resultWord =
       words[randomWord].charAt(0).toUpperCase() + words[randomWord].slice(1);
+    console.log(resultWord);
     setRandomWord(resultWord);
     setLogArray((word) => [...word, resultWord]);
   };
@@ -27,6 +42,15 @@ const WordGenerator = () => {
       <button className={cl.generate} onClick={wordHandler}>
         Generate
       </button>
+      <div className={cl["includes-box"]}>
+        <span className={cl.include}>Includes</span>
+        <input
+          type="text"
+          className={cl.input}
+          defaultValue={""}
+          ref={includeRef}
+        ></input>
+      </div>
       <div className={cl["log-box"]}>
         <ul>
           {logArray.map((numbers, i) => {
